e9625266b9aa4bf1ac71157dd78ee535c7c9df3e
Chu7zpah <chu7zpah@gmail.com>
2020-11-13 14:09:15 +0900

    test2

---
+++ test.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int t;
+    int a = 0; // 2-1
+
+    if(a == 0) {    // 2-2
+        t = (float) a;  //1
+    }
+    else    //2
+    {
+        t = 0;  //3
+    }
+
+    for (int i = 0 ; i < 5 ; i++)
+    {   //4
+        break;
+    }   //5
+    /*
+        1 
+    */
+    while(1){
+        if(a == 0){
+            break;
+        }
+
+        else{
+            float d;
+            d = (float)a;
+
+            break;
+        }
+    }
+    /*
+    2
+    */
+
+    if  (1)
+        return;
+    
+    return 0;
+}
+
